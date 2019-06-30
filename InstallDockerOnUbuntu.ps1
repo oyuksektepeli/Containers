2d5fa501fbba71a4d056bfe452604f4fdefebf9d
oyuksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-06-30 17:43:13 +0300

    docker install on ubuntu commands

---
+++ InstallDockerOnUbuntu.ps1
@@ -0,0 +1,32 @@
+#uninstall older versions
+$ sudo apt-get remove docker docker-engine docker.io containerd runc
+
+#get updates
+$ sudo apt-get update
+
+#install required packages
+$ sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+#Add Docker’s official GPG key:
+$ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+#Verify that you now have the key with the fingerprint 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88, by searching for the last 8 characters of the fingerprint.
+$ sudo apt-key fingerprint 0EBFCD88
+
+#set stable repository
+$ sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+#get updates
+$ sudo apt-get update
+
+#install docker
+$ sudo apt-get install docker-ce docker-ce-cli containerd.io
+

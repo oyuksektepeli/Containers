45060eb894b73f1b2e9c3b6bec65b3081444764a
oyuksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-06-30 13:27:42 +0300

    docker centos install added

---
+++ InstallDockeronCentos.ps1
@@ -0,0 +1,19 @@
+#Add required utilities to Centos
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+
+#Setup the Stable Repository
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+#Install Docker CE
+sudo yum -y install docker-ce
+
+
+#Enable and start Docker
+sudo systemctl start docker && sudo systemctl enable docker
+
+#Add customuser to the docker group
+sudo usermod -aG docker cloud_user
+
+#logout and login again to get permissions
+
+
---
+++ RemoveOlderDockerInstallsFromCentos.ps1
@@ -0,0 +1,8 @@
+sudo yum remove -y docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine

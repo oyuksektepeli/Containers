62c96fa112286c3fe556bf4c436ae3cc875ef461
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-08-01 19:54:29 +0300

    helm added

---
+++ helmrbaccommands.sh
@@ -0,0 +1,9 @@
+kubectl --namespace kube-system create serviceaccount tiller
+
+kubectl create clusterrolebinding tiller-cluster-rule \
+ --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+
+kubectl --namespace kube-system patch deploy tiller-deploy \
+ -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}' 
+
+ helm init --upgrade --service-account tiller
